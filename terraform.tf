1b3d77e3868e21aa5bdd8293a8438ff168276859
etafoh <33801223+etafoh@users.noreply.github.com>
2022-04-30 06:13:03 +0100

    Update terraform.tf

--- terraform.tf
+++ terraform.tf
@@ -1,4 +1,11 @@
 terraform {
+  cloud {
+    organization = "example-org-2c0f83"
+    workspaces {
+      name = "learn-terraform-resources"
+    }
+  }
+
   required_providers {
     aws = {
       source = "hashicorp/aws"
