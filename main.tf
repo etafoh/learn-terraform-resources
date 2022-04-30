63c69a67f4cb64ada9fd7f45f025db40c72ed5e7
etafoh <33801223+etafoh@users.noreply.github.com>
2022-04-30 04:42:34 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,15 @@
 provider "aws" {
-  region = "us-west-2"
+  access_key = var.access-key
+  secret_key = var.key-value 
+  region = "us-east-1"
+}
+
+variable "access-key" {
+  type = string  
+}
+
+variable "key-value" {
+  type = string  
 }
 
 provider "random" {}
@@ -7,11 +17,32 @@ provider "random" {}
 resource "random_pet" "name" {}
 
 resource "aws_instance" "web" {
-  ami           = "ami-a0cfeed8"
+  ami           = "ami-0f9fc25dd2506cf6d"
   instance_type = "t2.micro"
   user_data     = file("init-script.sh")
+  vpc_security_group_ids = [aws_security_group.web-sg.id]
 
   tags = {
     Name = random_pet.name.id
   }
 }
+
+resource "aws_security_group" "web-sg" {
+  name = "${random_pet.name.id}-sg"
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+
+
